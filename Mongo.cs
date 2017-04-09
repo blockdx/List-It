0bca529ace9bf3465fcd4002c774bc6943674194
Jim <Jim@JIM-PC>
2017-04-08 21:06:49 -0400

    Adding Mongo Code

---
+++ Mongo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Mongo
+{
+	public Mongo()
+	{
+        var creds = MongoCredential.CreateMongoCRCredential("ListDB", "ListAdmin", "Kimball1");
+        //Server settings
+        var settings = new MongoClientSettings
+        {
+            Credentials = new[] { creds },
+            Server = new MongoServerAddress("localhost")
+        };
+
+        //Get a reference to the Client Object
+        mongoClient = new MongoClient(settings);
+        mongoServer = mongoClient.GetServer();
+	}
+    public class User
+    {
+        [BsonId]
+        public ObjectId ID { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("Lists")]
+        public List<List> Lists { get; set; }
+    }
+    public class List
+    {
+        [BsonElement("List Name")]
+        public string ListName { get; set; }
+        [BsonElement("technology")]
+        public string List { get; set; }
+    }
+}
